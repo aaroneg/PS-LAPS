c9175c6b55d3e276425ebfa10251dc209f6a8614
Aaron <aaroneg@users.noreply.github.com>
2020-05-06 13:26:53 -0500

    Update get-laps-password-expiration.ps1

--- get-laps-password-expiration.ps1
+++ get-laps-password-expiration.ps1
@@ -2,6 +2,7 @@
 PARAM(
     [Parameter(Mandatory=$true,HelpMessage='What computer account will we check?')][string]$TargetComputer
 )
+# This line adds 1600 years because of the stupid way microsoft handles time.
 try {((Get-ADComputer -Identity $TargetComputer -Properties ms-mcs-admpwdexpirationtime).'ms-mcs-admpwdexpirationtime'|get-date).AddYears(1600)}
 catch [Microsoft.ActiveDirectory.Management.ADIdentityNotFoundException] {
     throw "This computer account could not be retrieved"
