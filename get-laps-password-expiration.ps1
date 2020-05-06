c2d7b5028793d6f6b4e29bb49e8a3097d0fef95e
Aaron <aaroneg@users.noreply.github.com>
2020-05-06 13:26:05 -0500

    Create get-laps-password-expiration.ps1

---
+++ get-laps-password-expiration.ps1
@@ -0,0 +1,9 @@
+[CmdletBinding()]
+PARAM(
+    [Parameter(Mandatory=$true,HelpMessage='What computer account will we check?')][string]$TargetComputer
+)
+try {((Get-ADComputer -Identity $TargetComputer -Properties ms-mcs-admpwdexpirationtime).'ms-mcs-admpwdexpirationtime'|get-date).AddYears(1600)}
+catch [Microsoft.ActiveDirectory.Management.ADIdentityNotFoundException] {
+    throw "This computer account could not be retrieved"
+}
+catch { throw "Unable to retrieve expiration time" }
